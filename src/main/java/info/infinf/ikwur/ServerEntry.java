27be5fc8bc394f046831accea8a6df3ce20138fb
MioMiko <728889@qq.com>
2023-12-19 00:52:56 +0800

    create

---
+++ src/main/java/info/infinf/ikwur/ServerEntry.java
@@ -0,0 +1,42 @@
+package info.infinf.ikwur;
+
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import com.mojang.brigadier.CommandDispatcher;
+import net.minecraft.command.CommandRegistryAccess;
+import net.minecraft.server.command.CommandManager;
+import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.server.PlayerManager;
+import net.minecraft.text.Text;
+
+public final class ServerEntry implements ModInitializer {
+	@Override
+	public void onInitialize() {
+		CommandRegistrationCallback.EVENT.register(ServerEntry::initCmd);
+	}
+
+	public static void initCmd(
+			CommandDispatcher<ServerCommandSource> dispatcher,
+			CommandRegistryAccess registryAccess,
+			CommandManager.RegistrationEnvironment env) {
+		dispatcher.register(CommandManager.literal("l")
+			.executes(ctx -> {
+				var sb = new StringBuilder();
+				for (var pl: ctx.getSource().getServer().getPlayerManager().getPlayerList()) {
+					sb.append(pl.getName().getString());
+					sb.append(": [");
+					var pos = pl.getBlockPos();
+					sb.append(pos.getX());
+					sb.append(',');
+					sb.append(pos.getY());
+					sb.append(',');
+					sb.append(pos.getZ());
+					sb.append("] ");
+					sb.append(pl.getWorld().getRegistryKey().getValue());
+					sb.append('\n');
+				}
+				ctx.getSource().sendFeedback(Text.of(sb.toString()), false);
+				return 1;
+			}));
+	}
+}
